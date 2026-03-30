eb481a8238ea4e2e007333ab2e523105146d43ca
Mikey Abela <mabela023@gmail.com>
2026-03-29 23:47:28 -0400

    bio update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,40 +5,20 @@ function AboutContent() {
     <div className="h-full flex flex-col items-center gap-3 overflow-scroll pb-10">
       <img src="/images/Sabel_Headshot.jpeg" className="lg:w-1/3 w-2/3" />
       <p className="font-regular lg:w-7/12 w-10/12">
-        Francesca Sabel is a Brooklyn-based director specializing in new play
-        development. Her work aims to be 'easy to enter and difficult to leave,'
-        emphasizing off-kilter rhythms, uncanny architectures, and structural
-        surprise. In addition to directing new work at venues including Ars
-        Nova, the Atlantic Theater Company, the Brick, Classic Stage, Clubbed
-        Thumb, and the Playwrights’ Realm, she has assisted (mostly on world
-        premieres) for directors like Knud Adams, Steve Broadnax, Neil Pepe, and
-        Caitlin Sullivan. She has been an Artist in Residence at Roundabout
-        Theater Company, Portland Theater Festival, the Cell, and the Tank
-        Artists’ Group. Previously, she led casting and community engagement as
-        Studio Theatre’s Creative Producer (DC). She is a full member of SDC,
-        and a graduate of Brown University, where she studied English and early
-        American history.
+        Francesca Sabel is a Brooklyn-based director of new plays. She has
+        directed and developed work with Ars Nova, the Atlantic Theater Company,
+        BEDLAM, the Brick, Classic Stage, Clubbed Thumb, the Working Theater and
+        the Playwrights’ Realm. She has also worked as assistant or associate to
+        directors including Anne Kauffman, Neil Pepe, Steve Broadnax, and Knud
+        Adams. Currently, she is developing new plays with Eliana
+        Theologides-Rodriguez, Kallan Dana, and Madison Fiedler.
       </p>
       <p className="font-regular lg:w-7/12 w-10/12">
-        Up next, Francesca is developing new plays by{" "}
-        <a
-          className="font-regular lg:w-7/12 w-10/12"
-          href="https://www.broadwayworld.com/off-broadway/article/Kallan-Dana-Named-Working-Theater-Playwright-In-Residence-20250814"
-        >
-          <u>Kallan Dana</u>
-        </a>{" "}
-        and{" "}
-        <a href="https://www.madisonfiedler.net/">
-          <u>Madison Fiedler</u>
-        </a>
-        , and serving as associate director to Annie Kauffman on Clare Barron’s{" "}
-        <a
-          className="font-regular lg:w-7/12 w-10/12"
-          href="https://cherrylanetheatre.org/shows/ygo"
-        >
-          <u>YOU GOT OLDER (Cherry Lane/A24)</u>
-        </a>
-        .
+        Francesca has been an Artist in Residence at Roundabout Theater Company,
+        Portland Theater Festival, the Cell, the 24 Hour Plays and TAG at the
+        Tank. Previously, she led casting and community engagement as Studio
+        Theatre’s Creative Producer (DC). She is a graduate of Brown University,
+        where she studied English and early American history.
       </p>
       <a
         href="/files/Sabel_Resume_Spring_25.pdf"
